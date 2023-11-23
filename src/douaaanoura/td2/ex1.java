58ad3f4de047c8de0947a55e7ce23a1e1abbf322
douaaanoura <douaaanoura0@gmail.com>
2023-11-23 19:38:57 +0100

    td2

--- src/douaaanoura/td2/ex1.java
+++ src/douaaanoura/td2/ex1.java
@@ -1,4 +1,30 @@
 package douaaanoura.td2;
 
 public class ex1 {
+    public static void main(String[] args) {
+        Point pt = new Point('A', 5.0);
+        pt.affiche();
+        pt.translate(2.0);
+        pt.affiche();
+    }
+    class Point {
+        char name;
+        double abscisse;
+
+        public Point(char name, double abscisse) {
+            name = this.name;
+            abscisse = this.abscisse;
+        }
+
+        public void affiche() {
+            System.out.println("Nom du point : " + name + " et abscisse du point est " + abscisse);
+        }
+
+        public void translate(double value) {
+            abscisse += value;
+        }
+    }
+
+
+
 }
